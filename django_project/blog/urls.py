f56d8d2c06171a533d0992494931e7064ed10608
mtrzebinski <mateusz.trzebinski.boss@gmail.com>
2019-06-09 17:16:22 +0200

    Dodawanie post

--- django_project/blog/urls.py
+++ django_project/blog/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from .views import NewsList, NewsCreate
 from . import views
 
 urlpatterns=[
-    path('', views.main_page, name='blog-main'),
+    path('', NewsList.as_view(), name='blog-main'),
     path('about/', views.descriptive_page, name='blog-about'),
+    path('post/new/', NewsCreate.as_view(), name='news-create'),
 ]
--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,62 +1,25 @@
 from django.shortcuts import render
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-from django.views.generic import (
-    ListView,
-    DetailView,
-    CreateView,
-    UpdateView,
-    DeleteView
-)
+from django.views.generic import ListView, CreateView
 from .models import News_table
 
 def main_page(task):
     news_dict={'news':News_table.objects.all()}
     return render(task, 'blog/main_page.html', news_dict)
 
-class PostListView(ListView):
+class NewsList(ListView):
     model = News_table
-    template_name = 'blog/home.html'  # <app>/<model>_<viewtype>.html
-    context_object_name = 'posts'
+    template_name = 'blog/main_page.html'  # <app>/<model>_<viewtype>.html
+    context_object_name = 'news'
     ordering = ['-date_posted']
 
-
-class PostDetailView(DetailView):
-    model = News_table
-
-
-class PostCreateView(LoginRequiredMixin, CreateView):
+class NewsCreate(LoginRequiredMixin, CreateView):
     model = News_table
     fields = ['title', 'content']
 
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
-
-
-class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
-    model = News_table
-    fields = ['title', 'content']
-
-    def form_valid(self, form):
-        form.instance.author = self.request.user
-        return super().form_valid(form)
-
-    def test_func(self):
-        post = self.get_object()
-        if self.request.user == post.author:
-            return True
-        return False
-
-
-class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
-    model = News_table
-    success_url = '/'
-
-    def test_func(self):
-        post = self.get_object()
-        if self.request.user == post.author:
-            return True
-        return False
-
+		
 def descriptive_page(task):
     return render(task, 'blog/descriptive_page.html', {'title':'About'})
